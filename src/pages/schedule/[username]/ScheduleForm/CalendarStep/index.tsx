6922171fd4cee72b099c6fb77104895228f0fc92
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-04-07 16:43:35 -0300

    Create style components ConfirmForm, FormHeader and FormError

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,37 @@
 import { Calendar } from '@/components/Calendar'
-import { Container } from './styles'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
 
 export function CalendarStep() {
+  const isDateSelected = true
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça feira <span>20 setembro</span>
+          </TimePickerHeader>
+          <TimePickerList>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
